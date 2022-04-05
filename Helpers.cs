ac1311a02d21745e893af5fe23df5470151f6c6e
Gabor <Gabor@MSI>
2022-04-05 07:54:29 -0400

    Add project files.

---
+++ Helpers.cs
@@ -0,0 +1,17 @@
+﻿namespace CombinationAndPermutationCalculator
+{
+    public static class Helpers
+    {
+        internal static int Factorial(int i)
+        {
+            int result = i < 0 ? -1 : i == 0 || i == 1 ? 1 : 1;
+
+            if (i > 0)
+            {
+                Enumerable.Range(1, i).ToList<int>().ForEach(element => result = result * element);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ OddsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CombinationAndPermutationCalculator
+{
+    public class OddsViewModel
+    {
+        [Required]
+        public int N { get; set; }
+
+        [Required]
+        public int R { get; set; }
+
+        [Required]
+        public bool IsOrderImportant { get; set; }
+
+        [Required]
+        public bool IsRepetitionAllowed { get; set; }
+
+        public double Result { get; set; }
+    }
+}
